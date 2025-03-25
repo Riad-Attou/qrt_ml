8cfb11ba5d0b3c1f6ff35c40887904b51651aa05
Mathé <mathe.marin22@gmail.com>
2025-03-25 08:25:45 +0100

    nouvelle courbe

--- src/main.py
+++ src/main.py
@@ -37,6 +37,38 @@ def plot_scores():
     plt.show()
 
 
+def plot_scores_depth():
+    nb_classes = 56
+    max_depth_fixed_range = list(range(1, 20, 1))
+    max_depth_cyto = 10
+
+    train_scores = []
+
+    for max_depth_fixed in max_depth_fixed_range:
+        print(
+            f"\nCalcul pour nb_classes = {nb_classes}, max_depth = {max_depth_fixed}, max_depth_cyto = {max_depth_cyto}"
+        )
+        merged_train, _, target_df = traitement_donnees(
+            nb_classes, max_depth_fixed, max_depth_cyto
+        )
+        train_ci_ipcw = modele_survival(
+            1, merged_train, merged_train, target_df
+        )
+        train_scores.append(train_ci_ipcw)
+
+    # Plot des scores en fonction de nb_classes
+    plt.figure(figsize=(10, 6))
+    plt.plot(
+        max_depth_fixed_range, train_scores, label="Train CI IPCW", marker="o"
+    )
+    plt.xlabel("Nombre de combinaisons de mutations (nb_max_depth)")
+    plt.ylabel("Score CI IPCW")
+    plt.title("Scores CI IPCW vs nb_max_depth pour nb_classes = 56")
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+
 if __name__ == "__main__":
     nb_classes = 56  # 39 pour opti score_train, 56 pour opti score_test
     max_depth = 15
@@ -52,4 +84,4 @@ if __name__ == "__main__":
 
     modele_survival(1, merged_train, merged_test, target_df)
 
-    # plot_scores()
+    plot_scores_depth()
